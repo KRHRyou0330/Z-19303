05e3ac2924a6a65fe3a30e5d0ed633abad8b017c
KRHRyou0330 <zm19303@josai.ac.jp>
2020-10-26 18:00:19 +0000

    10.26

--- ex6-5.js
+++ ex6-5.js
@@ -1,9 +1,12 @@
-function sayhello65() {
-var who = 'world';
- alert('Hello, ' + who + '!');
+var who65='world';
+function sayhello65(){
+ alert('Hello,'+ who65 +'!');
 }
-function taro() {
- who = '太郎';
-function hanako() {
- who = '花子';
+function taro65(){
+ who65 ='太郎';
+ alert(who65 +'が来た');
+}
+function hanako65(){
+ who65 ='花子';
+ alert(who65 + 'が来た');
 }
