1d957ae3c25d5cf0361acd2496089fc941ab837a
KRHRyou0330 <zm19303@josai.ac.jp>
2020-12-15 02:02:11 +0000

    12/15

--- ex06-8.js
+++ ex06-8.js
@@ -11,3 +11,7 @@ function hanako68() {
 function txtchange1() {
     document.getElementById('txt1').innerHTML= 'ナンバーワン！';
 }
+
+function imgchange1() {
+    document.getElementById('logo').src= './image/josailogo.jpg';
+}
